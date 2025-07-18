9031e265a6f7d733c485e74794cdd5813fd8c795
Vladut Cretu <vladutcretu98@gmail.com>
2025-07-18 20:35:52 +0300

    Update frontend to fetch the new endpoint

--- frontend/src/hooks/movies/movie/useReadUserMovies.jsx
+++ frontend/src/hooks/movies/movie/useReadUserMovies.jsx
@@ -7,29 +7,29 @@ import { movieService } from "@/services/movies/movieService"
 // Components here
 
 
-export const useGetMovies = () => {
+export const useReadUserMovies = (cityId) => {
   const [movies, setMovies] = useState([])
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
 
-  const getMovies = async () => {
+  const readUserMovies = async (cityId) => {
     try {
       setLoading(true)
       setError(null)
-      const response = await movieService.getMovies()
+      const response = await movieService.readUserMovies(cityId)
       setMovies(response.data)
-      console.log("Get Movies successful:", response.data)
+      console.log("Read User Movies successful:", response.data)
     } catch (error) {
       setError('Movies cannot be loaded. Please try again!')
-      console.error('Get Movies unsuccessful:', error)
+      console.error('Read User Movies unsuccessful:', error)
     } finally {
       setLoading(false)
     }
   }
   
   useEffect(() => {
-    getMovies()
-  }, [])
+    readUserMovies(cityId)
+  }, [cityId])
 
   return { movies, loading, error }
 }
