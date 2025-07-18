752c466d1ba709219537d91a9d9b51a1ca52db42
Vladut Cretu <vladutcretu98@gmail.com>
2025-07-18 16:49:39 +0300

    Add audit fields for Genre & Movie models

--- backend/movies/models.py
+++ backend/movies/models.py
@@ -5,6 +5,8 @@ from django.db import models
 
 class Genre(models.Model):
     name = models.CharField(max_length=55, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name_plural = "Genres"
@@ -15,13 +17,16 @@ class Genre(models.Model):
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=55)
+    title = models.CharField(max_length=55, unique=True)
     description = models.TextField(max_length=255)
     genres = models.ManyToManyField(Genre, related_name="movies")
     poster = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name_plural = "Movies"
+        ordering = ["-id"]
 
     def __str__(self):
-        return self.title
+        return f'"{self.title}" - created on: {self.created_at.strftime("%d %b %Y %H:%M:%S")}'
